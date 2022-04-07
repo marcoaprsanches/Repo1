824d9d186e21df2d2b449707e92595f6a17a1a60
Marco <marcoaprusso@gmail.com>
2022-04-07 09:16:04 -0300

    criei a pagina adote-aqui

---
+++ Trajetoria/meu-inicio/index.js
@@ -0,0 +1,7 @@
+console.log("Web Dev 2.0")
+console.log("Resilia")
+console.log("web Dev " + "Resilia " + "2.0")
+
+var nome = "Marco";
+var idade = 33;
+console.log ("nome" + " idade");
---
+++ Trajetoria/termometro/index.js
@@ -0,0 +1,6 @@
+var temperatura = 38;
+if (temperatura >= 38){
+console.log("Voce esta com febre");
+}else{
+    console.log("Nada de febre")
+}
